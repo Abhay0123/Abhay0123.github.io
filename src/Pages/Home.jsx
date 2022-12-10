f16e4197234c23e679c449bf2fc6b6517ae09990
Abhay Kumar <impathak038@gmail.com>
2022-12-10 16:07:11 +0530

    portfolio going on

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import {FaHome,FaHandHoldingHeart} from "react-icons/fa"
 import {BsHeartFill} from "react-icons/bs"
 
 import {AiOutlineTrademarkCircle} from "react-icons/ai"
-
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react'
 import {BsFillTelephoneFill} from "react-icons/bs"
 import {MdAttachEmail} from "react-icons/md"
 import { ImLinkedin, ImFolderDownload } from "react-icons/im";
@@ -105,10 +105,77 @@ const Home = () => {
         </Box>
         <Box> </Box>
       </SimpleGrid>
-      <Heading className="about">GitHub Status</Heading>
+   
       
     </>
+{/* Skills */}
+<>
+<Text className="about"  fontSize="5xl" as="b"> Tech Stack </Text>
+<Tabs>
+  <TabList>
+    <Tab><Text as='b'  fontSize="2xl">Frontend</Text></Tab>
+    <Tab><Text as='b'  fontSize="2xl">Backend</Text></Tab>
+    <Tab><Text as='b'  fontSize="2xl">Tools</Text></Tab>
+  </TabList>
+
+  <TabPanels>
+   
+    <TabPanel>
+     <SimpleGrid columns={6} spacing={10} className="tech">
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Box bg='black' borderRadius='md' w='140px' h='140px' className="hover">
+          <Image ml='18px'  boxSize='100px'  src='https://chiranjeev-thapliyal.vercel.app/svg/html-5.svg' alt='react'></Image><Text  align='center' fontWeight='800' className="text">HTML</Text></Box></Box>
+      
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://chiranjeev-thapliyal.vercel.app/svg/css3.svg' alt='react'></Image><Text align='center' fontWeight='800' className="text">CSS</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://chiranjeev-thapliyal.vercel.app/svg/javascript.svg' alt='react'></Image><Text align='center'fontWeight='800'  className="text">JAVASCRIPT</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://chiranjeev-thapliyal.vercel.app/svg/reactjs.svg' alt='react'></Image><Text align='center' className="text" fontWeight='800'>REACTJS</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'> <Image ml='20px' boxSize='100px' src='https://chiranjeev-thapliyal.vercel.app/svg/redux.svg' alt='react'></Image><Text align='center' className="text" fontWeight='800'>REDUX</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='30px' mt='5px' boxSize='80px' src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Vue.js_Logo_2.svg/120px-Vue.js_Logo_2.svg.png' alt='react'></Image><Text  align='center' mt='14px' className="text" fontWeight='800'>VUE</Text></Box>
+     </SimpleGrid>  
+    
+    </TabPanel>
+    <TabPanel>
+    <SimpleGrid columns={6} spacing={10} className="tech">
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Box bg='black' borderRadius='md' w='140px' h='140px' className="hover">
+          <Image ml='18px'  boxSize='100px'  src='https://chiranjeev-thapliyal.vercel.app/svg/nodejs.svg' alt='react'></Image><Text  align='center' fontWeight='800' className="text">NODEJS</Text></Box></Box>
+      
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='30px' mt='10px' boxSize='80px' src='https://www.pngfind.com/pngs/m/136-1363736_express-js-icon-png-transparent-png.png' alt='react'></Image><Text align='center' mt='9px' fontWeight='800' className="text">EXPRESSJS</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='30px' mt='10px' boxSize='80px' src='https://www.rlogical.com/wp-content/uploads/2021/08/Rlogical-Blog-Images-thumbnail.png' alt='react'></Image><Text align='center'fontWeight='800'  className="text" mt='8px'>NEXTJS</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://cdn.icon-icons.com/icons2/2415/PNG/512/mongodb_original_logo_icon_146424.png' alt='react'></Image><Text align='center' className="text" fontWeight='800'>MONGODB</Text></Box>
+    
+      
+     </SimpleGrid>  
+    </TabPanel>
+
+     <TabPanel>
+     <SimpleGrid columns={6} spacing={10} className="tech">
+      <Box display='flex' justifyContent='center' alignItems='center'>
+        <Box bg='black' borderRadius='md' w='140px' h='140px' className="hover">
+          <Image ml='18px'  boxSize='100px'  src='https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519' alt='react'></Image><Text  align='center' fontWeight='800' className="text">VS CODE</Text></Box></Box>
+      
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://cdn-icons-png.flaticon.com/512/5968/5968866.png' alt='react'></Image><Text align='center' fontWeight='800' className="text">GITHUB</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://cdn.iconscout.com/icon/free/png-256/code-sandbox-3772008-3151098.png' alt='react'></Image><Text align='center'fontWeight='800'  className="text">CODESANDBOX</Text></Box>
+      <Box className="hover" bg='black' borderRadius='md' w='140px' h='140px'><Image ml='20px' boxSize='100px' src='https://cutewallpaper.org/24/slack-logo-png/slack-logo-icon-download-in-colored-outline-style.png' alt='react'></Image><Text align='center' className="text" fontWeight='800'>SLACK</Text></Box>
+     
+     </SimpleGrid>  
+    </TabPanel>
+  </TabPanels>
+</Tabs>
+ 
+ 
+    
+    
+
+    
+    
+
+
+
+</>
+
 {/* github status */}
+<Text className="about"  fontSize="5xl" as="b">GitHub Status</Text>
 <Box display='flex' justifyContent='center' alignItems='center'>
   <GitHubCalendar
         username="Abhay0123"
